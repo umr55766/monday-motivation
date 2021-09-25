6576cd0b2760df0e03e26e53f95942882c6fc6b0
Umair Mohammad <umair.mohammad@ninjavan.co>
2021-09-25 16:10:47 +0530

    Migrate backend to fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,8 @@
-def app(event):
-    return "Hello, Monday motivation!"
+from fastapi import FastAPI
+
+app = FastAPI()  # notice that the app instance is called `app`, this is very important.
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
